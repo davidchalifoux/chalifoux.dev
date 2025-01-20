30d4bc219c17c5db5282d75f909298b91a4406a5
David Chalifoux <david@chalifoux.dev>
2025-01-20 15:02:54 -0500

    Fix hero on large screens

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,25 +9,29 @@ export const Hero: React.FC = () => {
 	return (
 		<div className="overflow-hidden bg-linear-to-b from-neutral-900 from-50% to-neutral-950">
 			<div className="container py-32 lg:py-64">
-				<div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-					<h1 className="max-w-2xl text-4xl font-bold tracking-tight text-neutral-100 sm:text-6xl lg:col-span-2 xl:col-auto">
-						Hey there!
-					</h1>
-					<div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-						<p className="text-lg leading-8 text-neutral-400">
-							My name is David Chalifoux.
-						</p>
-						<p className="text-lg leading-8 text-neutral-400">
-							I&apos;m a developer who loves the web.
-						</p>
-						<p className="text-lg leading-8 text-neutral-400">
-							You can learn more about my work below.
-						</p>
-						<ArrowDownIcon className="mt-6 h-6 w-6 text-yellow-300 animate-bounce" />
+				<div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:gap-x-8">
+
+					<div>
+						<h1 className="max-w-2xl text-4xl font-bold tracking-tight text-neutral-100 sm:text-6xl lg:col-span-2 xl:col-auto">
+							Hey there!
+						</h1>
+						<div className="mt-6 max-w-xl xl:col-end-1 xl:row-start-1">
+							<p className="text-lg leading-8 text-neutral-400">
+								My name is David Chalifoux.
+							</p>
+							<p className="text-lg leading-8 text-neutral-400">
+								I&apos;m a developer who loves the web.
+							</p>
+							<p className="text-lg leading-8 text-neutral-400">
+								You can learn more about my work below.
+							</p>
+							<ArrowDownIcon className="mt-6 h-6 w-6 text-yellow-300 animate-bounce"/>
+						</div>
 					</div>
+
 					<motion.div
-						initial={{ translateY: 600 }}
-						animate={{ translateY: 0 }}
+						initial={{translateY: 600}}
+						animate={{translateY: 0}}
 						className="perspective-distant"
 					>
 						<Image
